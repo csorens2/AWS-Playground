8cad27aeeaeda7b3942c1b885f9362d64b8fd0cf
Csorens2 <csorens2@users.noreply.github.com>
2026-06-22 18:16:03 -0500

    Removing API Scaffolding

--- BankingApp/lib/processor-stack.ts
+++ BankingApp/lib/processor-stack.ts
@@ -44,7 +44,7 @@ export class BankingAppProcessorStack extends cdk.Stack {
             }),
             vpc: processorVPC,
             vpcSubnets: {
-                subnetType: ec2.SubnetType.PUBLIC // TODO: Change to Isolated
+                subnetType: ec2.SubnetType.PRIVATE_ISOLATED
             },
             credentials: rds.Credentials.fromUsername(ledgerAdminName), // DO NOT TOUCH
             removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -83,10 +83,9 @@ export class BankingAppProcessorStack extends cdk.Stack {
 
         ledgerDatabase.secret!.grantRead(bankEventProcessorFunction.role!)
         ledgerDatabase.connections.allowFrom(bankEventProcessorFunction, ec2.Port.tcp(ledgerDatabase.instanceEndpoint.port))
-        ledgerDatabase.connections.allowDefaultPortFrom(ec2.Peer.ipv4("24.148.32.162/32")) // TODO: Remove me
         accountStatusDatabase.grantReadWriteData(bankEventProcessorFunction)
 
-        props.BankEventSQS.grantConsumeMessages(bankEventProcessorFunction) // TODO: Need?
+        props.BankEventSQS.grantConsumeMessages(bankEventProcessorFunction)
 
         bankEventProcessorFunction.addEventSource(new eventsources.SqsEventSource(props.BankEventSQS, {
             batchSize: 10,
@@ -94,41 +93,3 @@ export class BankingAppProcessorStack extends cdk.Stack {
         }));
     }
 }
-
-/*
-    const mySqlPort = 3306
-    const transactionLedgerDatabase = new rds.DatabaseCluster(this, 'TransactionLedger', {
-        engine: rds.DatabaseClusterEngine.auroraMysql({
-            version: rds.AuroraMysqlEngineVersion.VER_3_12_0,
-        }),
-        vpc: processorVPC,
-        vpcSubnets: {
-            subnetType: ec2.SubnetType.PUBLIC // TODO: Switch to Isolated when development is done
-        },
-        removalPolicy: cdk.RemovalPolicy.DESTROY,
-        writer: rds.ClusterInstance.serverlessV2('writer'),
-        readers: [rds.ClusterInstance.serverlessV2('reader', {
-            scaleWithWriter: true
-        })],
-
-        defaultDatabaseName: transactionLedgerDBName,
-        credentials: rds.Credentials.fromUsername(transactionLedgerAdminName), // DO NOT TOUCH
-        cloudwatchLogsExports: [ "general"],
-    });
-
-    const transactionLedgerProxy = new rds.DatabaseProxy(this, 'TransactionLedgerProxy', {
-        proxyTarget: rds.ProxyTarget.fromCluster(transactionLedgerDatabase),
-        vpc: processorVPC,
-        vpcSubnets: {
-            subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
-        },
-        iamAuth: true,
-        secrets: [transactionLedgerDatabase.secret!], // DO NOT TOUCH
-        clientPasswordAuthType: rds.ClientPasswordAuthType.MYSQL_NATIVE_PASSWORD,
-        debugLogging: true,
-    })
-
-    transactionLedgerDatabase.connections.allowFrom(transactionLedgerProxy, ec2.Port.tcp(mySqlPort))
-    transactionLedgerProxy.connections.allowFrom(bankEventProcessorFunction, ec2.Port.tcp(mySqlPort))
-    transactionLedgerProxy.grantConnect(bankEventProcessorFunction.role!, transactionLedgerAdminName)
- */
